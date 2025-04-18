396623e86dca2d4e2003be006d7d9ebf86519c4a
Kirill Suvorov <kirill_suvorov1@icloud.com>
2025-04-18 18:56:31 +0300

    Initial Commit

---
+++ SpeakIt/SpeakItApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakItApp.swift
+//  SpeakIt
+//
+//  Created by Kirill Suvorov on 18.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct SpeakItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
